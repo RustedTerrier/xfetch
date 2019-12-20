c973ea7f614f43b1c05372208d8c2e295daf466a
bit6tream <bit6stream@gmail.com>
2019-12-20 19:11:18 +0300

    some minor output changes

--- bitfetch.c
+++ bitfetch.c
@@ -6,22 +6,22 @@
 #include <pwd.h>                /* geteuid() */
 
 /* for colored output */
-#define COLOR_RED          "\x1b[31m"
-#define COLOR_GREEN        "\x1b[32m"
-#define COLOR_YELLOW       "\x1b[33m"
-#define COLOR_BLUE         "\x1b[34m"
-#define COLOR_MAGENTA      "\x1b[35m"
-#define COLOR_CYAN         "\x1b[36m"
-#define COLOR_WHITE        "\x1b[37m"
-#define COLOR_RESET        "\x1b[0m"
+#define COL_RED          "\x1b[31m"
+#define COL_GREEN        "\x1b[32m"
+#define COL_YELLOW       "\x1b[33m"
+#define COL_BLUE         "\x1b[34m"
+#define COL_MAGENTA      "\x1b[35m"
+#define COL_CYAN         "\x1b[36m"
+#define COL_WHITE        "\x1b[37m"
+#define COL_RES          "\x1b[0m"
 
-#define COLOR_RED_BOLD     "\x1b[31;1m"
-#define COLOR_GREEN_BOLD   "\x1b[32;1m"
-#define COLOR_YELLOW_BOLD  "\x1b[33;1m"
-#define COLOR_BLUE_BOLD    "\x1b[34;1m"
-#define COLOR_MAGENTA_BOLD "\x1b[35;1m"
-#define COLOR_CYAN_BOLD    "\x1b[36;1m"
-#define COLOR_WHITE_BOLD   "\x1b[37;1m"
+#define COL_RED_B        "\x1b[31;1m"
+#define COL_GREEN_B      "\x1b[32;1m"
+#define COL_YELLOW_B     "\x1b[33;1m"
+#define COL_BLUE_B       "\x1b[34;1m"
+#define COL_MAGENTA_B    "\x1b[35;1m"
+#define COL_CYAN_B       "\x1b[36;1m"
+#define COL_WHITE_B      "\x1b[37;1m"
 
 
 int main()
@@ -33,7 +33,7 @@ int main()
     FILE *fp = fopen("/etc/os-release", "r");
     if (fp == NULL)
     {
-        perror(COLOR_RED "Error: cannot read /etc/os-release." COLOR_RESET);
+        perror(COL_RED "Error: cannot read /etc/os-release." COL_RES);
         exit(1);
     }
     char distroName[64];
@@ -57,13 +57,13 @@ int main()
     username = pw -> pw_name;                /* get username */
 
     /* print all information */
-    printf(COLOR_MAGENTA "%s@%s\n" COLOR_RESET
-           COLOR_WHITE_BOLD "distro: " COLOR_RESET COLOR_MAGENTA "btw i use %s\n"        COLOR_RESET
-           COLOR_WHITE_BOLD "kernel: " COLOR_RESET COLOR_MAGENTA "%s\n"                  COLOR_RESET
-           COLOR_WHITE_BOLD "uptime: " COLOR_RESET COLOR_MAGENTA "%lih\n"                COLOR_RESET
-           COLOR_WHITE_BOLD "ram:    " COLOR_RESET COLOR_MAGENTA "%luM/%luM/%luM/%luM\n" COLOR_RESET
-           COLOR_WHITE_BOLD "swap:   " COLOR_RESET COLOR_MAGENTA "%luM/%luM\n"           COLOR_RESET
-           COLOR_WHITE_BOLD "procs:  " COLOR_RESET COLOR_MAGENTA "%d\n"                  COLOR_RESET,
+    printf(COL_MAGENTA "%s@%s\n" COL_RES
+           COL_WHITE_B "distro: " COL_RES COL_MAGENTA "btw i use %s\n"              COL_RES
+           COL_WHITE_B "kernel: " COL_RES COL_MAGENTA "%s\n"                        COL_RES
+           COL_WHITE_B "uptime: " COL_RES COL_MAGENTA "%lih\n"                      COL_RES
+           COL_WHITE_B "ram:    " COL_RES COL_MAGENTA "%lum / %lum / %lum / %lum\n" COL_RES
+           COL_WHITE_B "swap:   " COL_RES COL_MAGENTA "%lum / %lum\n"               COL_RES
+           COL_WHITE_B "procs:  " COL_RES COL_MAGENTA "%d\n"                        COL_RES,
            username, uinfo.nodename,
            distroName,
            uinfo.release,
