0d7da32ab1bec5a40d67920ea81f54aeb709cb07
bit9tream <bit6tream@cock.li>
2020-03-08 16:44:29 +0300

    added linux mint support

---
+++ distros/linuxmint.h
@@ -0,0 +1,22 @@
+#ifndef BITFETCH_MINT_H
+#define BITFETCH_MINT_H
+
+#include "colors.h"
+
+#define DISTRO_NAME "Linux Mint"
+#define COL_DIST   COL_GREEN
+#define COL_DIST_B COL_GREEN_B
+
+#define DISTRO_LOGO COL_DIST_B "\n"                 \
+                               " _____________\n"   \
+                               "|_            \\\n" \
+                               "  |  | _____  |\n"  \
+                               "  |  | | | |  |\n"  \
+                               "  |  | | | |  |\n"  \
+                               "  |  \\_____/  |\n" \
+                               "  \\___________/\n" \
+
+#define DISTRO_LOGO_LINE_COUNT "8"
+#define DISTRO_LOGO_WIDTH      "17"
+
+#endif
