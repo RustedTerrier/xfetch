711424c92042de4f40b3654bbafbcbf610b91cae
bit9tream <bit6tream@cock.li>
2020-03-08 17:32:43 +0300

    added manjaro support

---
+++ distros/manjaro.h
@@ -0,0 +1,22 @@
+#ifndef BITFETCH_H
+#define BITFETCH_H
+
+#include "colors.h"
+
+#define DISTRO_NAME "Manjaro Linux"
+#define COL_DIST    COL_GREEN
+#define COL_DIST_B  COL_GREEN_B
+
+#define DISTRO_LOGO COL_DIST_B "\n"                \
+                               " |||||||||  ||||\n" \
+                               " |||||||||  ||||\n" \
+                               " |||||      ||||\n" \
+                               " ||||| |||| ||||\n" \
+                               " ||||| |||| ||||\n" \
+                               " ||||| |||| ||||\n" \
+                               " ||||| |||| ||||\n"
+
+#define DISTRO_LOGO_LINE_COUNT "8"
+#define DISTRO_LOGO_WIDTH      "16"
+
+#endif
