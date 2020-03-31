a4edd82c46e2e012ea78f3e133e37d32683cfe4b
bit9tream <bit6tream@cock.li>
2020-03-31 22:59:52 +0300

    added openSUSE Leap

---
+++ distros/opensuse-leap.h
@@ -0,0 +1,22 @@
+#ifndef BITFETCH_H
+#define BITFETCH_H
+
+#include "colors.h"
+
+#define DISTRO_NAME "openSUSE Leap"
+#define COL_DIST    COL_GREEN
+#define COL_DIST_B  COL_GREEN_B
+
+#define DISTRO_LOGO COL_DIST_B "\n"                 \
+                               "     _______\n"     \
+                               " -___|   __ \\\n"   \
+                               "        / .\\ \\\n" \
+                               "        \\__/ |\n"  \
+                               "      _______|\n"   \
+                               "      \\_______\n"  \
+                               " --__________/\n"
+
+#define DISTRO_LOGO_LINE_COUNT "8"
+#define DISTRO_LOGO_WIDTH      "16"
+
+#endif
